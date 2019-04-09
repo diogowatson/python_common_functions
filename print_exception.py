36ed5bdbf9c3e5d1916e06bcf8bc4a415f5a3b20
Diogo Watson <diogowatson@gmail.com>
2019-04-09 08:53:01 -0400

    Create print_exception.py

---
+++ print_exception.py
@@ -0,0 +1,18 @@
+import os, sys
+
+def print_exception(e):
+    '''
+    print an customized exception
+    param@:
+        e   -Required : exeception string
+        
+    usage:
+        - Use function after an except Exception catch
+        example:
+        except Exeption as e:
+            print_exception(e)
+    '''
+    
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+    print(exc_type, fname, exc_tb.tb_lineno, e)
