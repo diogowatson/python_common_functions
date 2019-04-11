484e0a8341ba1278ef261759b86d434bb0288717
Diogo Watson <diogowatson@gmail.com>
2019-04-11 08:55:05 -0400

    Create file_len.py

---
+++ file_len.py
@@ -0,0 +1,14 @@
+def file_len(fname):
+    '''
+    Returns the number of lines of a file
+    @param:
+         - fname    - Required : file name and path (str) 
+    
+    @return:
+         - Number of lines (int) 
+    '''
+    
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
