ecbb5042ee061ae52663ebf797f5fbdca21673bb
Diogo Watson <diogowatson@gmail.com>
2019-04-11 09:08:22 -0400

    Create read_lines.py
    Read an N number of lines of a file and save it in a new file

---
+++ read_lines.py
@@ -0,0 +1,15 @@
+read_lines(number_of_lines, file, output_file):
+'''
+Read an N number of lines of a file and save it in a new file
+@param:
+    number_of_lines    - Required : number of lines to be read (int)
+    file               - Required : file to be read (str)
+    output_file        - Required : output file (str)
+'''
+with open(file) as f, open(output_file) as out:
+    
+    head= [next(f) for x in xrange(number_of_lines)]
+    out.write(head)
+    
+    out.close()
+    f.close()
