e9085068079fa6cac7b34e13482a943e83ca5e70
Diogo Watson <diogowatson@gmail.com>
2019-04-15 11:48:25 -0400

    Update save_dataframe.py
    changed function name

--- save_dataframe.py
+++ save_dataframe.py
@@ -2,13 +2,13 @@ import pandas as pd
 from datetime import datetime as dt
 
 
-def times_as_string():
+def current_time():
     #returns current date in string format
 
     return dt.now().strftime('%Y-%m-%d %H %M %S')
 
 
-def save_dataframe_as_excel(df, outputFile, sheetName='sheet1'):
+def df_to_excel(df, outputFile, sheetName='sheet1'):
     '''save an pandas DataFrame to an Excel file
        @params:
            df            - Required : DataFrame to be converted (pandas dataFrame)
@@ -19,10 +19,10 @@ def save_dataframe_as_excel(df, outputFile, sheetName='sheet1'):
     try:
         #.xlsx already in the outputfile string
         if '.xlsx' in outputFile:
-            outputFile = outputFile +"_" + times_as_string()
+            outputFile = outputFile +"_" + current_time()
         else:
             #in case user forgot '.xlsx extension'
-            outputFile = outputFile +"_" + times_as_string()+ ".xlsx"
+            outputFile = outputFile +"_" + current_time()+ ".xlsx"
 
         writer = pd.ExcelWriter(outputFile, engine='xlsxwriter')
         df.to_excel(writer, sheetName)
