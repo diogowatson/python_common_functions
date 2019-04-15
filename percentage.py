51d3dde033114816b498ed1a5605ba8a677f303b
Diogo Watson <diogowatson@gmail.com>
2019-04-15 13:36:46 -0400

    Update percentage.py
    fixed bugs

--- percentage.py
+++ percentage.py
@@ -1,19 +1,18 @@
 def percentage(x, y):
-    '''
+    """
     Give the percentage of a value base on a total value
     @params:
         x   - Required : value of percentage to be obtained
-        y   - Required : total value from whre percentage will be obtained
-    
+        y   - Required : total value from where percentage will be obtained
+
     @return:
        - percentage value as string (str)
-       - if any of the values is zero the operatin can't be performed
+       - if any of the values is zero the operation can't be performed
          then, the function will return 'N/A'
-    '''
+    """
     if x != 0 and y != 0:
-        percentage = lambda x, y: int(((x) /y) * 100)
+        result = float(((x) / y) * 100)
         
-        return '%' + str(percentage(x, y))
+        return '%' + '{0:.2f}'.format(result)  # return only 2 number after .
     else:
-        
-        return 'N/A'
+        return '%0'
