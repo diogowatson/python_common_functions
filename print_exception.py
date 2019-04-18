7239f814c6cd3753c9808372b288e236ce236e33
Diogo Watson <diogowatson@gmail.com>
2019-04-18 13:39:07 -0400

    Update print_exception.py

--- print_exception.py
+++ print_exception.py
@@ -1,7 +1,7 @@
 import os, sys
 
 def print_exception(e):
-    '''
+    """
     print an customized exception
     param@:
         e   -Required : exeception string
@@ -11,7 +11,7 @@ def print_exception(e):
         example:
         except Exeption as e:
             print_exception(e)
-    '''
+    """
     
     exc_type, exc_obj, exc_tb = sys.exc_info()
     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
