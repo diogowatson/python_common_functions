f485a90d9c4a50908cce18819517c4a3260b0739
Diogo Watson <diogowatson@gmail.com>
2019-04-18 13:46:10 -0400

    Update file_len.py

--- file_len.py
+++ file_len.py
@@ -1,12 +1,12 @@
 def file_len(fname):
-    '''
+    """
     Returns the number of lines of a file
     @param:
          - fname    - Required : file name and path (str) 
     
     @return:
          - Number of lines (int) 
-    '''
+    """
     
     with open(fname) as f:
         for i, l in enumerate(f):
